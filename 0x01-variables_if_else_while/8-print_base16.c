8db0f765c42929590539cbf174010c0a126b6a12
Taha Nour El-Dein <81038277+tahammnour@users.noreply.github.com>
2022-09-10 02:06:20 +0200

    fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - entry point
- *
- * Return: Always 0 (success/correct)
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
  */
 int main(void)
-
 {
-char base_16;
+	int c;
+	int d = 0;
 
-for (base_16 = 48; base_16 <= 57; base_16++)
-{putchar(base_16);
-}
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
 
-for (base_16 = 97 ; base_16 <= 102; base_16++)
-{
-putchar(base_16);
-}
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
-putchar ('\n');
-
-return (0);
-
-}
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
