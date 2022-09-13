09ccb99cac97dd532cd02c183a93fc510484810e
tahammnour <tahammnour@outlook.com>
2022-09-13 23:44:39 +0000

    change number value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ * _abs - get value then * 
+ * @n: param int
+ * Return: new value n * number or n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -10);
+	}
+	else
+	{
+		return (n);
+	}
+}
