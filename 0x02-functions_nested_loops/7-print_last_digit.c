768a9cb2e7f8f14c7366433c736aa2ba33e8c6f7
tahammnour <tahammnour@outlook.com>
2022-09-13 23:51:25 +0000

    last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+
+#include "main.h"
+/**
+ * print_last_digit - print last number
+ * @n: param int
+ * Return: value of r
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	if (n < 0)
+	{
+		r = -1 * (n % 10);
+		_putchar(r + '0');
+		return (r);
+	}
+	else
+	{
+		r = n % 10;
+		_putchar(r + '0');
+		return (r);
+	}
+}
