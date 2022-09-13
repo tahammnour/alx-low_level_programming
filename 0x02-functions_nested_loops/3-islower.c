543018873cea24074dc86fa155fa35ec89bac9f4
tahammnour <tahammnour@outlook.com>
2022-09-13 23:31:59 +0000

    print alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - print int 0 or 1
+ * @c: parameter int
+ *
+ * Description: print from a to z
+ * Return: alpahabet
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
