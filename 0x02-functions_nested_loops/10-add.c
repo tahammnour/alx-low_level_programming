ede2c44d5379bb54c2285ecde526f15e5e3023f5
tahammnour <tahammnour@outlook.com>
2022-09-14 00:01:35 +0000

    sum 2 num

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+
+/**
+ * add - sum to number
+ * @a: first pram call a
+ * @b: first pram call b
+ *
+ * Return: sum a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
