59ba8bc9c9f74cbdc30bbe16785201590bfec9eb
tahammnour <tahammnour@outlook.com>
2022-09-15 23:17:38 +0000

    multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies a * b
+ * @a: a param int
+ * @b: b param int
+ * Return: num contain a * b
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	num = a * b;
+
+	return (num);
+}
