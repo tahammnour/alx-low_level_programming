1cc5b3a592407a2ee9a1275db5cab76bca95ed72
tahammnour <tahammnour@outlook.com>
2022-09-15 19:40:44 +0000

    change from 98 to 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - set number to positive_or_negative(i) 
+* Description: know if number positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
