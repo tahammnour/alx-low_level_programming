4bedd90b426dc71441b9e23b6ae8132d148bdfbd
tahammnour <tahammnour@outlook.com>
2022-09-15 23:04:37 +0000

    isupper = 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - check char upercase
+ * @c: the param which is char c
+ * Return: 1 if it upper and 0 for lower in main.c
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
