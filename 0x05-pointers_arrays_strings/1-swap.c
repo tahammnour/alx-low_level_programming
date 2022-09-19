7411595f36b2d79a785e03ddbe73c536d314cfa1
tahammnour <tahammnour@outlook.com>
2022-09-19 22:51:54 +0000

    get value from another

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - store 2var value revrese
+ *@a: pointer for a var
+ *@b: pointer for b var
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+int storVar;
+
+storVar = *a;
+*a = *b;
+*b = f;
+}
