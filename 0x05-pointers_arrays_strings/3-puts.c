8ec9280907e21f97ef073410346eb3e5b293c3e8
Taha Nour El-Dein <81038277+tahammnour@users.noreply.github.com>
2022-09-20 01:06:23 +0200

    put function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,14 @@
 #include "main.h"
-/**
-*_puts - display text from _puts with new line
-*@str: pointer store in c var
-*
-*Return: nothing
-*/
 
+/**
+* _puts -> this is a function that puts
+* @str: a param to _puts function
+*/
 void _puts(char *str)
 {
-char *c;
-int m;
-
-c = str;
-
-for (m = 0; c[m]; m++)
-{
-_putchar (c[m]);
-}
-_putchar('\n');
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
 }
