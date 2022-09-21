a043db20f50d84e1879fbccf10fe61eefbbb64b2
tahammnour <tahammnour@outlook.com>
2022-09-21 21:49:48 +0000

    fix betty Return

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _strncpy - print depent on number char
+ * * _strncpy - print depend on number char
  * @dest: pointer char
  * @src: pointer char
  * @n: return num with char to print
+ *
+ * Return: address of dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
