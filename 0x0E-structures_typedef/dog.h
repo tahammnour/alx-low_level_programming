e6766067a691c370d2932e90dca683877bcaed73
tahammnour <tahammnour@outlook.com>
2022-10-10 22:24:14 +0000

    fix header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,20 +7,17 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
 int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif /* _DOG_H_ */
