4b9b3ae92af59df20b0138a063eeb850e2280ffa
tahammnour <tahammnour@outlook.com>
2022-10-13 23:25:56 +0000

    print any

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -17,4 +17,5 @@ int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif /* _VARIADIC_FUNCTIONS_H_ */
