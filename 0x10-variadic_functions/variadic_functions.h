8a4a14cfc6ac7359410f6b3ec277c1b658cf2813
tahammnour <tahammnour@outlook.com>
2022-10-13 23:23:10 +0000

    print num

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,4 +15,5 @@ typedef struct print
 
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif /* _VARIADIC_FUNCTIONS_H_ */
