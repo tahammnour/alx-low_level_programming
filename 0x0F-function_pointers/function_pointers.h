0b49b843e77a2569788ec921137da31b2adf9fdc
tahammnour <tahammnour@outlook.com>
2022-10-13 00:39:31 +0000

    excute func

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,5 @@
 #include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char);
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif/* _FUNCTION_POINTERS_H_ */
