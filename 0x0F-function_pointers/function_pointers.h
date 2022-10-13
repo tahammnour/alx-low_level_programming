a7c2139f9aa6ce3a603644203978e6c873b682aa
tahammnour <tahammnour@outlook.com>
2022-10-13 00:35:00 +0000

    print func name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+#include <stddef.h>
+void print_name(char *name, void (*f)(char *));
+int _putchar(char);
+
+#endef /* _FUNCTION_POINTERS_H_ */
