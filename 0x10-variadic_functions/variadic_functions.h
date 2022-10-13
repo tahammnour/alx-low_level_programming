0807d6e7638e7909fced4be7ccf6ff7a4bb7f933
tahammnour <tahammnour@outlook.com>
2022-10-13 23:21:04 +0000

    add struct

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,12 @@
  * @t: print type
  * @f: print function
  */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 #endif /* _VARIADIC_FUNCTIONS_H_ */
