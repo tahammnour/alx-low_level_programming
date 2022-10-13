ea3133b8a85ad6a9a4943de653877f736552cdec
tahammnour <tahammnour@outlook.com>
2022-10-13 23:15:58 +0000

    sum param

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#include <stdarg.h>
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+#endif /* _VARIADIC_FUNCTIONS_H_ */
